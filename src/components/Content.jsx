2c86099ed4e6e844391e22c9803762ec6d29d77c
Emil <mikayelyanemil19@gmail.com>
2024-07-02 15:54:01 +0400

    feat: add main category tabs

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,12 @@
 import '@/assets/Content.css'
 import React from 'react'
+import MainCategory from '@/components/MainCategory'
 
 export default function Content() {
-  return (
-    <div className='main-content'></div>
-  )
+    return (
+        <div className='main-content'>
+            <h2>Schedule an Appointment</h2>
+            <MainCategory />
+        </div>
+    )
 }
